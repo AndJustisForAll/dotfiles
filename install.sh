30ed78caf59d9719855ca81786212b385f03c556
amunoz <amunozdv@gmail.com>
2018-02-05 23:44:01 -0600

    update install.sh add oh-my-zsh.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+echo Installing Oh-My-Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo Removing default oh-my-zsh.sh
+rm ~/.oh-my-zsh/oh-my-zsh.sh
+echo Creating Symlink to ~/.oh-my-zsh/oh-my-zsh.sh
+ln -s ~/dotfiles/oh-my-zsh.sh ~/.oh-my-zsh/oh-my-zsh.sh
 echo Installing vim-plug
 #https://github.com/junegunn/vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
@@ -8,3 +14,7 @@ echo Creating ~/.config/nvim
 echo Creating Symlink to ~/.config/nvim/init.vim
 ln -s ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
 
+echo Installing pgcli
+if ! [ -x "$(command -v pgcli)" ]; then
+  pip install -U pgcli
+fi
